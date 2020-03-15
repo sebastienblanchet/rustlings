d452be7813aacbdbb97ec10f004c4dc1ac86f8b0
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-15 16:55:59 -0400

    Section 8 enums, pretty cool and complicated

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -1,11 +1,16 @@
 // enums2.rs
 // Make me compile! Execute `rustlings hint enums2` for hints!
 
-// I AM NOT DONE
+// List 6-2
 
 #[derive(Debug)]
 enum Message {
-    // TODO: define the different variants used below
+    // enums can be structs, string, tuples or nil
+    Move{ x : i32, y : i32},
+    Echo(String),
+    ChangeColor(i32, i32, i32),
+    Quit,
+
 }
 
 impl Message {
--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,11 +1,22 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Quit, // takes in nothing
+    Move{x: u8, y : u8}, // anonymous struct
+    Echo(String),
+    ChangeColor(u8, u8, u8),
 }
+
+// above is similar to doing it all with a struct
+// struct QuitMessage; // unit struct
+// struct MoveMessage {
+//     x: i32,
+//     y: i32,
+// }
+// struct WriteMessage(String); // tuple struct
+// struct ChangeColorMessage(i32, i32, i32); // tuple struct
 
 struct Point {
     x: u8,
@@ -18,6 +29,8 @@ struct State {
     quit: bool
 }
 
+// impl is essentally the fxn definitions of a class
+// METHOD DEFINITON WORKS FOR BOTH struct and enum
 impl State {
     fn change_color(&mut self, color: (u8, u8, u8)) {
         self.color = color;
@@ -37,6 +50,39 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        // essentially a switch case
+        // but its pretty cool that you can do it on a type?
+        match message {
+            // ChangeColor => self.change_color(message.ChangeColor),
+            Message::ChangeColor(r, g, b) => {
+                self.change_color((r,g,b));
+            }
+
+            Message::Move{x, y} => {
+                // create a new point
+                let s = Point{
+                    x : x,
+                    y : y,
+                };
+
+                // pass into self call
+                self.move_position(s);
+            }
+
+            Message::Quit => {
+                self.quit();
+            }
+
+            Message::Echo(string) => {
+                self.echo(string);
+            }
+
+            // this is default
+            _ => {
+                println!("Something else");
+            }
+        }
+        
     }
 }
 
@@ -51,6 +97,7 @@ mod tests {
             position: Point{ x: 0, y: 0 },
             color: (0, 0, 0)
         };
+
         state.process(Message::ChangeColor(255, 0, 255));
         state.process(Message::Echo(String::from("hello world")));
         state.process(Message::Move{ x: 10, y: 15 });
