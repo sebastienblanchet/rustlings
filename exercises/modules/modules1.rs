308948e5b8af350f1dd87315acbe741ddd7ba0c9
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-15 17:43:27 -0400

    Section 7ish modules

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -1,14 +1,17 @@
 // modules1.rs
 // Make me compile! Execute `rustlings hint modules1` for hints :)
 
-// I AM NOT DONE
-
+// name space
 mod sausage_factory {
-    fn make_sausage() {
+    pub fn make_sausage() {
         println!("sausage!");
     }
 }
 
 fn main() {
+    // fn ==> private
+    // deal with this
+    // use super::*;
+    // no way of colling the function without pub
     sausage_factory::make_sausage();
 }
--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -1,12 +1,12 @@
 // modules2.rs
 // Make me compile! Execute `rustlings hint modules2` for hints :)
 
-// I AM NOT DONE
-
 mod delicious_snacks {
-    use self::fruits::PEAR as fruit;
-    use self::veggies::CUCUMBER as veggie;
-
+    // straight up namespace
+    // This 
+    pub use self::fruits::PEAR      as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
+    
     mod fruits {
         pub const PEAR: &'static str = "Pear";
         pub const APPLE: &'static str = "Apple";
