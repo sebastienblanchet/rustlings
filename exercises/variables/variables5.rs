33a3410d89b1824e69839bdac58323a779aa4eeb
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-15 12:37:29 -0400

    Work through variables

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,14 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
 
-// I AM NOT DONE
+// https://doc.rust-lang.org/book/ch03-02-data-types.html
 
 fn main() {
     let number = "3";
     println!("Number {}", number);
-    number = 3;
-    println!("Number {}", number);
+    // number = 3;
+    
+    // wtf can have emojis
+    let heart_eyed_cat = '😻';
+    println!("Number {}", heart_eyed_cat);
 }
