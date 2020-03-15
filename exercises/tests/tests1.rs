479fc7c0388b8383fd36b3f5e84e55604cec53d3
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-15 17:16:14 -0400

    Sect 11 internal unit tests

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -6,12 +6,28 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests1` for hints :)
 
-// I AM NOT DONE
+// this is to configure unit tests
+// would run this `cargo tests`
+// 
+// Section 11.3 The Book 
+// https://doc.rust-lang.org/book/ch11-03-test-organization.html?highlight=tests#integration-tests
+// 
+
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
 
 #[cfg(test)]
 mod tests {
+    // allows to test private functions
+    use super::*;
+
     #[test]
     fn you_can_assert() {
-        assert!();
+        assert!(4 == internal_adder(2, 2));
     }
 }
--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -2,12 +2,25 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
 
-// I AM NOT DONE
+
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
 
 #[cfg(test)]
 mod tests {
+
+    // could also just ask for exact name space function
+    // similar to cpp
+
+    use internal_adder;
+
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
